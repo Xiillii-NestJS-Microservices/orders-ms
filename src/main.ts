8475a6d00b47070aaa211c20f980e648a6089646
Jose Alonso <mail@josealonso.dev>
2025-04-17 01:33:16 -0600

    Add Orders controller as microservice
    npm i --save @nestjs/microservices
    
    nest g res orders --no-spec

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Orders-Main');
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+  await app.listen();
   logger.log(`ORDERS Microservice running on port ${envs.port}`);
 }
 bootstrap();
