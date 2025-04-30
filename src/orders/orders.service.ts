9af0105531b921c8e5dca46e52f4ea71ae400e25
Jose Alonso <mail@josealonso.dev>
2025-04-29 20:26:53 -0600

    Create communication betweet products MS and Orders microservice

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,12 +1,26 @@
-import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from 'generated/prisma';
-import { RpcException } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { ChangeOrderStatusDto, OrderPaginationDto } from './dto';
+import { PRODUCT_SERVICE } from 'src/config';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('OrderService');
+
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {
+    super();
+  }
 
   async onModuleInit() {
     await this.$connect();
@@ -14,10 +28,22 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     this.logger.log('Database connected');
   }
 
-  create(createOrderDto: CreateOrderDto) {
-    return this.order.create({
-      data: createOrderDto,
-    });
+  async create(createOrderDto: CreateOrderDto) {
+    const ids = [5, 600];
+
+    const products = await firstValueFrom(
+      this.productsClient.send({ cmd: 'validate_products' }, ids),
+    );
+
+    return products;
+
+    // return {
+    //   service: 'Orders Microservice',
+    //   createOrderDto: createOrderDto,
+    // };
+    // return this.order.create({
+    //   data: createOrderDto,
+    // });
   }
 
   async findAll(paginationDto: OrderPaginationDto) {
